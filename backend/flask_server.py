48f4c4a4b35491057fbdf7b0de48e925b08165ee
Daniel Benitez-Lopez <dnlbntzlpz@gmail.com>
2024-12-23 18:19:38 +0900

    Motor Flask server working

--- backend/flask_server.py
+++ backend/flask_server.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from motor_control import motor_forward, motor_backward, motor_stop
+import os
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="../frontend/templates",
+    static_folder="../frontend/static")
 
 @app.route("/")
 def index():
@@ -13,7 +17,7 @@ def control():
     """Handle control commands sent from the frontend."""
     data = request.json
     action = data.get("action")
-    speed = data.get("speed", 1.0)  # Default speed
+    speed = data.get("speed", 0.9)  # Default speed
 
     if action == "forward":
         motor_forward(speed)
